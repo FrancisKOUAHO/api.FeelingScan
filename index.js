3469b31fe48a9504f6fc6e10f05513323f583b48
Arboos-naori <kouaho@arboos.com>
2021-10-21 15:54:17 +0200

    node

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // call all the required packages
 import express from "express"
-import multer from "multer"
-import fileExtension from "file-extension"
 import cors from "cors"
 import dotenv from "dotenv"
+import morgan from "morgan"
 
 import {Picture} from "./src/models/dataPicture"
 import {connection} from "./src/config/connectToDatabase"
+import {upload} from "./src/middleware/upload";
+import {getPicture, savePicture} from "./src/controllers/upload.controller";
 
 
 dotenv.config()
@@ -18,100 +19,21 @@ const app = express();
 
 // cors allow usage of server from different origin only for development
 app.use(cors())
-
-
-// Configure Storage
-const storage = multer.diskStorage({
-
-    // Setting directory on disk to save uploaded files
-    destination: function (req, file, cb) {
-        cb(null, 'uploads')
-    },
-
-    // Setting name of file saved
-    filename: function (req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now() + '.' + fileExtension(file.originalname))
-    }
-})
-
-
-const upload = multer({
-    storage: storage,
-    limits: {
-        // Setting Image Size Limit to 2MBs
-        fileSize: 2000000
-    },
-    fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-            //Error
-            cb(new Error('Please upload JPG and PNG images only!'))
-        }
-        //Success
-        cb(undefined, true)
-    }
-})
+app.use(morgan('dev'))
 
 
 //ROUTES WILL GO HERE
 app.get('/', function (req, res) {
-    res.json({message: 'Server Started!'});
+    res.json({message: 'Le serveur a démarré !'});
 });
 
 
-app.post('/uploadfile', upload.single('uploadedImage'), (req, res, next) => {
-    const file = req.file
-    console.log(req);
-
-    let saveFile = new Picture({
-        fieldname: req.file.fieldname,
-        originalname: req.file.originalname,
-        encoding: req.file.encoding,
-        mimetype: req.file.mimetype,
-        destination: req.file.destination,
-        filename: req.file.filename,
-        path: req.file.path,
-        size: req.file.size,
-    })
-    saveFile.save()
-
-    if (!file) {
-        const error = new Error('Please upload a file')
-        error.httpStatusCode = 400
-        return next(error)
-    }
-    res.status(200).send({
-        statusCode: 200,
-        status: 'success',
-        uploadedFile: file
-    })
-
-}, (error, req, res, next) => {
-    res.status(400).send({
-        error: error.message
-    })
-})
+app.post('/uploadFilePicture', upload.single('uploadedImage'), savePicture)
 
 
-app.get('/getPicture', (req, res, next) => {
-    Picture.find((error, picture) => {
-        if (error) {
-            return res.status(500).json(
-                {
-                    success: false,
-                }
-            );
-        } else {
-            return res.status(201).json(
-                {
-                    data: picture,
-                    success: true,
-                }
-            );
-        }
-    })
-})
+app.get('/getPicture', getPicture)
 
 
 connection()
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Le serveur a démarré sur le port ${PORT}`));
---
+++ src/controllers/upload.controller.js
@@ -0,0 +1,57 @@
+import {Picture} from "../models/dataPicture";
+
+const savePicture = (req, res, next) => {
+    const file = req.file
+    console.log(req);
+
+    let saveFile = new Picture({
+        fieldname: req.file.fieldname,
+        originalname: req.file.originalname,
+        encoding: req.file.encoding,
+        mimetype: req.file.mimetype,
+        destination: req.file.destination,
+        filename: req.file.filename,
+        path: req.file.path,
+        size: req.file.size,
+    })
+    saveFile.save()
+
+    if (!file) {
+        const error = new Error('Veuillez télécharger un fichier')
+        error.httpStatusCode = 400
+        return next(error)
+    }
+    res.status(200).send({
+        statusCode: 200,
+        status: 'success',
+        uploadedFile: file
+    })
+
+}
+
+
+const getPicture = (req, res, next) => {
+    Picture.find((error, picture) => {
+        if (error) {
+            return res.status(500).json(
+                {
+                    success: false,
+                }
+            );
+        } else {
+            return res.status(201).json(
+                {
+                    data: picture,
+                    success: true,
+                }
+            );
+        }
+    })
+}
+
+
+
+export {
+    savePicture,
+    getPicture
+}
